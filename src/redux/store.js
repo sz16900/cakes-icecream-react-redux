489b9dccc5ff2b362ae3faf577c1299af198b8a1
sz16900 <sz16900@my.bristol.ac.uk>
2020-09-08 11:49:19 -1000

    add more middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './rootReducer';
 import logger from 'redux-logger';
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 export default store;
