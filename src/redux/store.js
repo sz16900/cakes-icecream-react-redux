22c22c233bf34a35cb12ca7cff7973f8fb9ceda7
sz16900 <sz16900@my.bristol.ac.uk>
2020-09-08 11:36:44 -1000

    add logger middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './rootReducer';
+import logger from 'redux-logger';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
